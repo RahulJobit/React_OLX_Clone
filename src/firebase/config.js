1baf3c9aaa7064e08192bb32b21deba3b11e091b
RahulJobit <rahuljobittelma007@gmail.com>
2022-07-25 13:03:34 +0530

    the remote name changed and the file add to my repo and content updated

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA1JW2x_fk6r7SCUsSLfCSxNWz_GEX6IUM",
+    authDomain: "olx-clone-98df5.firebaseapp.com",
+    projectId: "olx-clone-98df5",
+    storageBucket: "olx-clone-98df5.appspot.com",
+    messagingSenderId: "788479572554",
+    appId: "1:788479572554:web:be05bae1582cd11d3784b3",
+    measurementId: "G-NMCJT5X8ZC"
+  };
+ 
+export default firebase.initializeApp(firebaseConfig)
